764c9f9ecf53b1059207b3d1f779f1e2492410c6
yevhen31546 <dobryanskiy.yevhen@gmail.com>
2019-11-16 13:44:04 -0800

    update 20191116

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,5 @@
- //= require jquery-1.12.4.min.js
- //= require bootstrap.min.js
- //= require fontawesome.js
- //= require jquery.counterup.min.js
- //= require jquery.magnific-popup.min.js
- //= require nav.js
- //= require slick.min.js
- //= require custom.js
+//= require jquery-1.12.4.min.js
+//= require bootstrap.min.js
+//= require fontawesome.js
+//= require slick.min.js
+//= require custom.js
